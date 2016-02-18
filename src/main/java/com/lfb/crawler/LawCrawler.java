9355aaaaa1e1db3034589166b571c0e8dd66fbb2
lifenbo <249532343@qq.com>
2016-02-18 19:31:14 +0800

    annotaion crawler worked

--- src/main/java/com/lfb/crawler/LawCrawler.java
+++ src/main/java/com/lfb/crawler/LawCrawler.java
@@ -1,6 +1,6 @@
 package com.lfb.crawler;
 
-import java.util.Date;
+import java.util.List;
 
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.model.OOSpider;
@@ -9,71 +9,35 @@ import us.codecraft.webmagic.model.annotation.ExtractBy.Type;
 import us.codecraft.webmagic.model.annotation.HelpUrl;
 import us.codecraft.webmagic.model.annotation.TargetUrl;
 import us.codecraft.webmagic.pipeline.FilePageModelPipeline;
+import us.codecraft.webmagic.scheduler.FileCacheQueueScheduler;
 
 @TargetUrl(value = { "http://www.chinalawedu.com/falvfagui/\\d+/\\w+.shtml" })
 @HelpUrl("http://www.chinalawedu.com/falvfagui/21752/")
 public class LawCrawler {
 
-	@ExtractBy("//div[@id='fontzoom']/p[strong/text()='a']/text()")
+	@ExtractBy(value = "<strong>发文单位</strong>：(.*?)</p>", type = Type.Regex)
 	private String publisher;
 	
-//	@ExtractBy("//div[@id='fontzoom']/p[strong='文　　号']/text()")
-//	private String docnum;
+	@ExtractBy(value = "<strong>文  号</strong>：(.*?)</p>", type = Type.Regex)
+	private String docnum;
 //	
-//	@ExtractBy("//div[@id='fontzoom']/p[strong='发布日期']/text()")
-//	private String publishDate;
+	@ExtractBy(value = "<strong>发布日期</strong>：(.*?)</p>", type = Type.Regex)
+	private String publishDate;
 //	
-//	@ExtractBy("//div[@id='fontzoom']/p[strong='生效日期']/text()")
-//	private String validSince;
-	
-//	@ExtractBy(value="", type=Type.Css)
-//	private String title;
-//	
-//	@ExtractBy(value="", type=Type.Css)
-//	private String content;
+	@ExtractBy(value = "<strong>生效日期</strong>：(.*?)</p>", type = Type.Regex)
+	private String validSince;
 
-	public String getPublisher() {
-		return publisher;
-	}
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
-//	public String getDocnum() {
-//		return docnum;
-//	}
-//	public void setDocnum(String docnum) {
-//		this.docnum = docnum;
-//	}
-//	public String getPublishDate() {
-//		return publishDate;
-//	}
-//	public void setPublishDate(String publishDate) {
-//		this.publishDate = publishDate;
-//	}
-//	public String getValidSince() {
-//		return validSince;
-//	}
-//	public void setValidSince(String validSince) {
-//		this.validSince = validSince;
-//	}
-//	public String getTitle() {
-//		return title;
-//	}
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-//	public String getContent() {
-//		return content;
-//	}
-//	public void setContent(String content) {
-//		this.content = content;
-//	}
-	
+	@ExtractBy(value="//div[@class='hd tc']/h3/text()")
+	private String title;
+
+	@ExtractBy(value="//p[contains(@style,'TEXT-INDENT: 2em')]/text()")
+	private List<String> content;
+
 	public static void main(String[] args){
 		
 		OOSpider.create(Site.me().setSleepTime(1000),  new FilePageModelPipeline("./data/"), LawCrawler.class)
+			.setScheduler(new FileCacheQueueScheduler("./monitor/"))
 			.addUrl("http://www.chinalawedu.com/falvfagui/21752/").thread(1).run();
-		
 	}
 	
 }
